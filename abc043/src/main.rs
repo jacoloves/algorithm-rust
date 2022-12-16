97bebea507a0b2801354981e2d2c05b2b4a12f48
jacoloves <5511068t@gmail.com>
2022-12-16 22:05:26 +0900

    20221216

--- abc043/src/main.rs
+++ abc043/src/main.rs
@@ -1,6 +1,37 @@
 use proconio::input;
 
 fn main() {
+    b();
+}
+
+fn b() {
+    input! {
+        s: String,
+    };
+
+    let mut ans: Vec<char> = Vec::new();
+
+    for i in s.as_str().chars() {
+        if i == '0' {
+            ans.push('0')
+        } else if i == '1' {
+            ans.push('1')
+        } else {
+            if ans.is_empty() {
+            } else {
+                ans.pop();
+            }
+        }
+    }
+
+    for i in ans {
+        print!("{}", i);
+    }
+    println!();
+}
+
+#[allow(dead_code)]
+fn a() {
     input! {
         n: i32,
     };
