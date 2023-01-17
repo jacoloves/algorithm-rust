a08f8ecd3768a9b9553884f92f05c591c191fb86
jacoloves <5511068t@gmail.com>
2023-01-17 17:54:15 +0900

    20230117

--- 2023/1/week1/src/main.rs
+++ 2023/1/week1/src/main.rs
@@ -11,161 +11,27 @@ where
 }
 
 fn main() {
-    //itp1_1_a();
-    //itp1_1_b();
-    //itp1_1_c();
-    //itp1_1_d();
-    //itp1_2_a();
-    //itp1_2_b();
-    //itp1_2_c();
-    //itp1_2_d();
-    //itp1_3_a();
-    //itp1_3_b();
-    itp1_3_c();
+    itp1_4_b();
 }
 
-fn itp1_3_c() {
-    let mut av: Vec<i32> = Vec::new();
-    let mut bv: Vec<i32> = Vec::new();
-    loop {
-        let line: Vec<i32> = read_line();
-        if line[0] == 0 && line[1] == 0 {
-            break;
-        }
+fn itp1_4_b() {
+    let line: Vec<f64> = read_line();
 
-        av.push(line[0]);
-        bv.push(line[1]);
-    }
-
-    for i in 0..av.len() {
-        if av[i] > bv[i] {
-            println!("{} {}", bv[i], av[i]);
-        } else {
-            println!("{} {}", av[i], bv[i]);
-        }
-    }
-}
-
-fn itp1_3_b() {
-    let mut i: i32 = 1;
-
-    let mut ans: Vec<String> = Vec::new();
-    loop {
-        let line: Vec<i32> = read_line();
-        if line[0] == 0 {
-            break;
-        }
-        let ele: String = format!("case {}: {}", i, line[0]);
-        ans.push(ele);
-        i = i + 1;
-    }
-
-    for e in ans.iter() {
-        println!("{}", e);
-    }
-}
-
-fn itp1_3_a() {
-    for _ in 1..=1000 {
-        println!("Hello World");
-    }
-}
-
-fn itp1_2_d() {
-    let line: Vec<i32> = read_line();
-
-    let w: i32 = line[0];
-    let h: i32 = line[1];
-    let x: i32 = line[2];
-    let y: i32 = line[3];
-    let r: i32 = line[4];
-
-    if w < x + r || h < y + r || 0 > x - r || 0 > y - r {
-        println!("No");
-    } else {
-        println!("Yes");
-    }
-}
-
-fn itp1_2_c() {
-    let mut line: Vec<i32> = read_line();
-
-    line.sort();
-
-    let a: i32 = line[0];
-    let b: i32 = line[1];
-    let c: i32 = line[2];
-
-    println!("{} {} {}", a, b, c);
-}
-
-fn itp1_2_b() {
-    let line: Vec<i32> = read_line();
-    let a: i32 = line[0];
-    let b: i32 = line[1];
-    let c: i32 = line[2];
-
-    if a < b && b < c {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
-}
-
-fn itp1_2_a() {
-    let line: Vec<i32> = read_line();
-    let a: i32 = line[0];
-    let b: i32 = line[1];
-
-    if a < b {
-        println!("a < b");
-    } else if a > b {
-        println!("a > b");
-    } else {
-        println!("a == b");
-    }
-}
-
-fn itp1_1_d() {
-    let line: Vec<i32> = read_line();
-    let mut sec: i32 = line[0];
-
-    let hour: i32 = sec / 3600;
-    sec = sec % 3600;
-
-    let minute: i32 = sec / 60;
-    sec = sec % 60;
-
-    println!("{}:{}:{}", hour, minute, sec);
-}
-
-fn itp1_1_c() {
-    let line: Vec<i32> = read_line();
-    let mut area: i32 = 1;
-    let mut circle: i32 = 0;
-
-    for e in line.iter() {
-        area = area * e;
-        circle = circle + (e * 2);
-    }
+    let area: f64 = line[0].powf(2.0) * std::f64::consts::PI;
+    let circle: f64 = 2.0 * std::f64::consts::PI * line[0];
 
     println!("{} {}", area, circle);
 }
 
-fn itp1_1_b() {
+fn itp1_4_a() {
     let line: Vec<i32> = read_line();
 
-    let num: i32 = line[0];
+    let a: i32 = line[0];
+    let b: i32 = line[1];
 
-    let mut ans: i32 = 1;
+    let d: i32 = a / b;
+    let r: i32 = a % b;
+    let f: f64 = a as f64 / b as f64;
 
-    for _ in 1..=3 {
-        ans = ans * num;
-    }
-
-    println!("{}", ans);
-}
-
-fn itp1_1_a() {
-    println!("Hello World");
+    println!("{} {} {}", d, r, f);
 }
