2d5835034c3528bd8932a889c203c7111798c0bc
jacoloves <5511068t@gmail.com>
2023-01-19 07:13:57 +0900

    20230119

--- 2023/1/week1/src/main.rs
+++ 2023/1/week1/src/main.rs
@@ -11,7 +11,171 @@ where
 }
 
 fn main() {
-    itp1_4_c();
+    itp1_6_a();
+}
+
+fn itp1_6_a() {
+    let _: Vec<i32> = read_line();
+    let mut line: Vec<i64> = read_line();
+
+    line.reverse();
+
+    for i in 0..line.len() {
+        if i == line.len() - 1 {
+            print!("{}", line[i]);
+        } else {
+            print!("{} ", line[i]);
+        }
+    }
+
+    println!();
+}
+
+fn itp1_5_d() {
+    let line: Vec<i32> = read_line();
+    let mut ans: Vec<String> = Vec::new();
+
+    let mut i: i32 = 0;
+    loop {
+        i = i + 1;
+        // END_CHECK_NUM
+        if i > line[0] {
+            break;
+        }
+        // CHECK_NUM
+        let mut x: i32 = i.clone();
+        if x % 3 == 0 {
+            ans.push(format!(" {}", i));
+            continue;
+        }
+        // INCLUDE3
+        loop {
+            if x % 10 == 3 {
+                ans.push(format!(" {}", i));
+                break;
+            }
+
+            if x == 0 {
+                break;
+            }
+
+            x = x / 10;
+        }
+    }
+
+    for e in ans.iter() {
+        print!("{}", e);
+    }
+    println!();
+}
+
+fn itp1_5_c() {
+    let mut wv: Vec<i32> = Vec::new();
+    let mut hv: Vec<i32> = Vec::new();
+    loop {
+        let line: Vec<i32> = read_line();
+        if line[0] == 0 && line[1] == 0 {
+            break;
+        }
+
+        hv.push(line[0]);
+        wv.push(line[1]);
+    }
+
+    let mut reverse: bool;
+    for i in 0..hv.len() {
+        for j in 1..=hv[i] {
+            if j % 2 != 0 {
+                reverse = true;
+            } else {
+                reverse = false;
+            }
+            for k in 1..=wv[i] {
+                if reverse {
+                    if k % 2 != 0 {
+                        print!("#");
+                    } else {
+                        print!(".");
+                    }
+                } else {
+                    if k % 2 != 0 {
+                        print!(".");
+                    } else {
+                        print!("#");
+                    }
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+}
+
+fn itp1_5_b() {
+    let mut wv: Vec<i32> = Vec::new();
+    let mut hv: Vec<i32> = Vec::new();
+    loop {
+        let line: Vec<i32> = read_line();
+        if line[0] == 0 && line[1] == 0 {
+            break;
+        }
+
+        hv.push(line[0]);
+        wv.push(line[1]);
+    }
+
+    for i in 0..hv.len() {
+        for j in 1..=hv[i] {
+            for k in 1..=wv[i] {
+                if j == 1 || j == hv[i] {
+                    print!("#");
+                } else {
+                    if k == 1 || k == wv[i] {
+                        print!("#");
+                    } else {
+                        print!(".");
+                    }
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+}
+
+fn itp1_5_a() {
+    let mut wv: Vec<i32> = Vec::new();
+    let mut hv: Vec<i32> = Vec::new();
+    loop {
+        let line: Vec<i32> = read_line();
+        if line[0] == 0 && line[1] == 0 {
+            break;
+        }
+
+        hv.push(line[0]);
+        wv.push(line[1]);
+    }
+
+    for i in 0..hv.len() {
+        for _ in 1..=hv[i] {
+            for _ in 1..=wv[i] {
+                print!("#");
+            }
+            println!();
+        }
+        println!();
+    }
+}
+
+fn itp1_4_d() {
+    let _: Vec<i32> = read_line();
+    let line: Vec<i64> = read_line();
+
+    let max_num: i64 = *line.iter().max().unwrap();
+    let min_num: i64 = *line.iter().min().unwrap();
+    let sum_num: i64 = line.iter().sum();
+
+    println!("{} {} {}", min_num, max_num, sum_num);
 }
 
 fn itp1_4_c() {
