50d10b1fb66b6c1ae7b3f4e9da7b2780be1dbaaf
shooonng <5511068t@gmail.com>
2025-04-12 22:20:14 +0900

    20250412

--- 2025/tutorial_atcoder/src/main.rs
+++ 2025/tutorial_atcoder/src/main.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::collections::HashSet;
 
+#[allow(unused_imports)]
 use std::iter::zip;
 #[allow(unused_imports)]
 use std::{isize, usize};
@@ -39,8 +40,173 @@ where
 }
 /* ↑AOJ */
 
+const MOD: usize = 1_000_000_000;
+
 fn main() {
-    abc169a();
+    abc401c();
+}
+
+#[allow(dead_code)]
+fn dfs(idx: usize, k: usize, n: usize, s: &[u8], pos: &mut Vec<usize>, all: &mut Vec<Vec<usize>>) {
+    if pos.len() == k {
+        all.push(pos.clone());
+        return;
+    }
+    if idx >= n {
+        return;
+    }
+
+    dfs(idx + 1, k, n, s, pos, all);
+
+    if s[idx] == b'.' {
+        return;
+    }
+    if !pos.is_empty() && idx <= pos[pos.len() - 1] + 1 {
+        return;
+    }
+
+    pos.push(idx);
+    dfs(idx + 1, k, n, s, pos, all);
+    pos.pop();
+}
+
+#[allow(dead_code)]
+fn abc401d() {
+    const DOT: u8 = b'.';
+    const O: u8 = b'o';
+    const Q: u8 = b'?';
+
+    input! {
+        n: usize,
+        k: usize,
+        s: [u8; n],
+    }
+
+    let mut count_o = vec![0; n];
+    let mut count_dot = vec![0; n];
+    let mut total = 0;
+
+    let mut positions = vec![];
+
+    let mut all_valid_o_positions = vec![];
+    dfs(0, k, n, s, &mut positions, &mut all_valid_o_positions);
+
+    'outer: for o_pos in all_valid_o_positions {
+        for (i, &c) in s.iter().enumerate() {
+            if c == O && !o_pos.contains(&i) {
+                continue 'outer;
+            }
+            if c == DOT && o_pos.contains(&i) {
+                continue 'outer;
+            }
+        }
+        total += 1;
+        let mut temp = vec![DOT; n];
+        for &i in &o_pos {
+            temp[i] = O;
+        }
+        for i in 0..n {
+            match temp[i] {
+                O => count_o[i] += 1,
+                DOT => count_dot[i] += 1,
+                _ => {}
+            }
+        }
+    }
+
+    let mut res = vec![b'?'; n];
+    for i in 0..n {
+        if count_o[i] = total {
+            res[i] = b'o';
+        } else if count_dot[i] = total {
+            res[i] = b'.';
+        }
+    }
+
+    println!("{}", String::from_utf8(res).unwrap());
+}
+
+#[allow(dead_code)]
+fn abc401c() {
+    input! {
+        n: usize,
+        k: usize
+    }
+
+    let mut a = vec![0usize; n + 1];
+    let mut s = vec![0usize; n + 2];
+
+    for i in 0..=n {
+        if i < k {
+            a[i] = 1;
+        } else {
+            a[i] = (s[i] + MOD - s[i - k]) % MOD;
+        }
+        s[i + 1] = (s[i] + a[i]) % MOD;
+    }
+
+    println!("{}", a[n]);
+}
+
+#[allow(dead_code)]
+fn abc401b() {
+    input! {
+        n: usize,
+        s_arr: [String; n],
+    }
+
+    let mut login_flg = false;
+
+    let mut flg_false_private_access_cnt = 0;
+
+    for s in s_arr {
+        if s == "login" {
+            login_flg = true;
+            continue;
+        }
+
+        if s == "logout" {
+            login_flg = false;
+            continue;
+        }
+
+        if s == "private" {
+            if !login_flg {
+                flg_false_private_access_cnt += 1;
+            }
+            continue;
+        }
+    }
+
+    println!("{}", flg_false_private_access_cnt);
+}
+
+#[allow(dead_code)]
+fn abc401a() {
+    input! {
+        s: usize,
+    }
+
+    if s >= 200 && s <= 299 {
+        println!("Success");
+        return;
+    }
+
+    println!("Failure");
+}
+
+#[allow(dead_code)]
+fn abc086a() {
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    if a * b % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }
 
 #[allow(dead_code)]
